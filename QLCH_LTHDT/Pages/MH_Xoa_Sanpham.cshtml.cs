b3aac237588f235b28bab2d540288344525f5dd7
Madeleine912 <89530435+Madeleine912@users.noreply.github.com>
2024-05-09 16:38:00 +0700

    Add files via upload

---
+++ QLCH_LTHDT/Pages/MH_Xoa_Sanpham.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QLCH_LTHDT_Entities;
+using QLCH_LTHDT_Services;
+using System.Reflection;
+
+namespace QLCH_LTHDT_WEB.Pages
+{
+    public class MH_Xoa_SanphamModel : PageModel
+
+    {
+        [BindProperty]
+        public int masp { get; set; }
+
+        public string Chuoi { get; set; } = string.Empty;
+        private ITXulysanpham _xulysanpham = new Xulysanpham();
+
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+
+            try
+           
+                {
+                    var spxoa = new Sanpham(masp.ToString());
+
+                    _xulysanpham.Xoasanpham(spxoa);
+                    Response.Redirect("/MH_Danhsach_Sanpham");
+                }
+            
+
+            catch (Exception ex)
+            {
+                Chuoi = ex.Message;
+            }
+
+
+
+
+        }
+    }
+}
+
+
+
